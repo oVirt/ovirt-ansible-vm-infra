56b6f090ebc9f8c6b3bbefb3bb3e66ef8edc3df5
Ondra Machacek <omachace@redhat.com>
2018-08-29 12:35:21 +0200

    Package filter_plugins
    Change-Id: I7db2f6e1285d4235b23b26983cf7161e8c3f351f
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- build.sh
+++ build.sh
@@ -46,6 +46,7 @@ install() {
   cp -pR defaults/ $PKG_DATA_DIR
   cp -pR library/ $PKG_DATA_DIR
   cp -pR tasks/ $PKG_DATA_DIR
+  cp -pR filter_plugins/ $PKG_DATA_DIR
 
   echo "Installation done."
 }
