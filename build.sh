a2f48da8d387378210290d8f58f672c2318375d3
Ondra Machacek <omachace@redhat.com>
2019-08-27 13:31:38 +0200

    Release 1.1.21 (#108)

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 VERSION="1.1.21"
-MILESTONE=master
-RPM_RELEASE="0.1.$MILESTONE.$(date -u +%Y%m%d%H%M%S)"
+MILESTONE=
+RPM_RELEASE="1"
 
 ROLE_NAME="ovirt.vm-infra"
 PACKAGE_NAME="ovirt-ansible-vm-infra"
